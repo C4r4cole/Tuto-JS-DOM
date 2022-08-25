3cf5cfa3a2203909958bbcaecd5a19ab10d18998
Caracole <francoismoulin37@gmail.com>
2022-08-25 20:41:41 +0200

    form events tuto JS DOM

--- index.js
+++ index.js
@@ -31,5 +31,70 @@ window.addEventListener("mousemove", (e) => {
 });
 
 window.addEventListener("mousedown", () => {
-  mousemove.style.transform = "scale(2)";
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
 });
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  mousemove.style.border = "2px solid teal";
+});
+
+capitale.addEventListener("mouseenter", () => {
+  capitale.style.background = "rgba(0, 0, 0, 0.6";
+});
+
+capitale.addEventListener("mouseout", () => {
+  capitale.style.background = "pink";
+});
+
+response.addEventListener("mouseover", () => {
+  response.style.transform = "rotate(2deg)";
+});
+
+//----------------------------------------------------------
+//Keypress event
+
+const keypressContainer = document.querySelector(".keypress");
+const key = document.getElementById("key");
+
+const ring = (key) => {
+  const audio = new Audio();
+  audio.src = key + ".mp3";
+  audio.play();
+};
+
+document.addEventListener("keypress", (e) => {
+  key.textContent = e.key;
+
+  if (e.key === "k") {
+    keypressContainer.style.background = "blue";
+  } else if (e.key === "t") {
+    keypressContainer.style.background = "red";
+  } else {
+    keypressContainer.style.background = "teal";
+  }
+
+  if (e.key === "z") {
+    ring(e.key);
+  }
+});
+
+//---------------------------------------------------------------
+//Scroll event
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 120) {
+    nav.style.top = "0px";
+  } else {
+    nav.style.top = "-50px";
+  }
+});
+
+//-------------------------------------------------------------
+// Form Events
+
+const inputname = document.querySelector('input[type="text"]');
+
+console.log(inputname);
